7816fc5e18a3f7d2a35f4b1572a7e6afa5baf083
Codeofsanju <sanjeev.sharma90@myhunter.cuny.edu>
2018-08-11 14:43:46 -0400

    Learning some JSX syntax
    1. Adjacent Tags
    2. Expressions
    3. Objects
    4. Conditional Rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,18 +2,83 @@
 
 console.log('App.js is running');
 
+var app = {
+    title: 'Pick4me!',
+    subtitle: 'We pick for you :)',
+    options: ['one', 'two']
+};
+
 //JSX - JavasScript XML
 var template = React.createElement(
-    'h1',
+    'div',
     null,
-    'Pick4Me'
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' ',
+        app.options.length > 0 ? 'Here are your options: ' + app.options : 'There are no options',
+        ' '
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            ' Item one '
+        ),
+        React.createElement(
+            'li',
+            null,
+            ' Item two '
+        )
+    )
 );
-/*var template = React.createElement(
-    "p",
-    { id: "someid" },
-    "This is JSX from app.js!"
-); this is the same as  var template = <p> This is JSX from app.js!</p> but your browser does not undrstand JSX, 
-   so we need babel to compile down JSX to regular JavaScript, which is what this is, just JavaScript*/
+
+var user = {
+    userName: 'Sanjeev Sharma',
+    userAge: 25,
+    userLocation: 'NYC'
+};
+
+function getLocation(location) {
+    if (location) {
+        // if location does exist
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location,
+            ' '
+        );
+    }
+}
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.userName ? user.userName : 'Anonymous'
+    ),
+    user.userAge && user.userAge >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.userAge
+    ),
+    getLocation(user.userLocation)
+);
 
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,13 +1,47 @@
 console.log('App.js is running');
 
+
+var app = {
+    title: 'Pick4me!',
+    subtitle: 'We pick for you :)',
+    options: ['one', 'two',],
+};
+
+
 //JSX - JavasScript XML
-var template = <h1>Pick4Me</h1>;
-/*var template = React.createElement(
-    "p",
-    { id: "someid" },
-    "This is JSX from app.js!"
-); this is the same as  var template = <p> This is JSX from app.js!</p> but your browser does not undrstand JSX, 
-   so we need babel to compile down JSX to regular JavaScript, which is what this is, just JavaScript*/  
+var template = (
+    <div>
+        <h1>{app.title}</h1> 
+        {app.subtitle && <p>{app.subtitle}</p>}   
+        <p> {app.options.length > 0 ?'Here are your options: ' + app.options : 'There are no options'} </p>
+        <ol> 
+            <li> Item one </li>
+            <li> Item two </li>
+        </ol>
+    </div>
+);
+
+
+
+
+var user = {
+    userName: 'Sanjeev Sharma',
+    userAge: 25,
+    userLocation: 'NYC'
+};
+
+function getLocation(location){
+    if (location) { // if location does exist
+        return <p>Location: {location} </p>;
+    }
+}
+var templateTwo = (
+    <div>
+        <h1>{user.userName ? user.userName : 'Anonymous'}</h1>
+        {(user.userAge && user.userAge >= 18) && <p>Age: {user.userAge}</p>}
+        {getLocation(user.userLocation)}
+    </div>
+);
 
 var appRoot = document.getElementById('app');
 
