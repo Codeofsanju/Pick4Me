85072e1c42cd8174072fd164ff58c06372a7573d
Codeofsanju <sanjeev.sharma90@myhunter.cuny.edu>
2018-08-14 21:03:44 -0400

    arguments and this in ES6 arrow functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,43 +1,79 @@
 'use strict';
 
-// es5 function
-var square = function square(x) {
-    return x * x;
-};
-console.log(square(8));
+// arguments objects - no longer bound with arrow functions
 
-// named es5 function
-function squareNamed(x) {
-    return x * x;
+var add = function add(a, b) {
+    //ES5
+    console.log(arguments); // returns all arguments passed to this function, irregardless to what is listed as an argument. 
+    return a + b;
 };
-console.log(squareNamed(9));
+console.log(add(9, 19));
 
-// es6 arrow function
-var squareArrow = function squareArrow(x) {
-    // notice that the function name is now anonymous, so you cannot define a function by name and you must use variable
-    return x * x;
+var addArrow = function addArrow(a, b) {
+    // ES6 arrow function
+    //console.log(arguments); Not possible with arrow functions
+    return a + b;
 };
-console.log(squareArrow(10));
+console.log(addArrow(20, 2));
 
-//arrow function expression syntax 
-var squareArrowExp = function squareArrowExp(x) {
-    return x * x;
+// this keyword - no longer bound
+var user = {
+    name: 'Sanjeev',
+    cities: ['NYC', 'Queens', 'Miami'],
+    printPlaceLived: function printPlaceLived() {
+        // ES5: since this function is added to the object property, the this is bound to that object
+        console.log(this.name);
+        console.log(this.cities);
+
+        this.cities.forEach(function (city) {
+            //console.log(this.name + ' has lived n' + city); // does not work since this anonymous function is not bound to the object
+        });
+    }
 };
-console.log(squareArrowExp(11));
 
-// Challenge - Use arrow functions
-// getFirstName('Sanjeev Sharma') -> Sanjeev
-// Create regular arrow function
-// Create second arrow function to do same using expression syntax
+console.log(user.printPlaceLived());
 
-// PART I: Regular arrow syntax
-var getFirstName = function getFirstName(x) {
-    return x.split(' ')[0];
+var user1 = {
+    name1: "Sanjeev1",
+    cities1: ['NYC', 'Queens', 'Miami'],
+    printPlaceLived: function printPlaceLived() {
+        var _this = this;
+
+        // ES6 syntax for defining a method function
+        this.cities1.forEach(function (city) {
+            console.log(_this.name1 + 'has lived in ' + city); // ES6 works since arrow functions inherit the this of the context they are created
+        });
+    }
 };
-console.log('RegFname: ', getFirstName('Sanjeev Sharma'));
 
-// PART II: Expression syntax
-var getFirstNameExp = function getFirstNameExp(x) {
-    return x.split(' ')[0];
+user1.printPlaceLived();
+
+var user2 = { //using map
+    name2: 'Sanjeev2',
+    cities2: ['NYC', 'Queens', 'Miami'],
+    printPlaceLived: function printPlaceLived() {
+        var _this2 = this;
+
+        return this.cities2.map(function (city) {
+            return _this2.name2 + ' has lived in ' + city;
+        }); //map allow you to transform each item in the arry and get a new array back
+    }
 };
-console.log('ExpFname: ', getFirstNameExp('Sanjeev Sharma'));
+console.log(user2.printPlaceLived());
+
+// CHALLENGE
+var multiplier = {
+    // number - array of numbers
+    numbers: [1, 2, 3],
+    // multiplyBy - single number 
+    multiplyBy: 2,
+    // Method: Multiply -> returns a array where the numbers have been multiplied
+    multiply: function multiply() {
+        var _this3 = this;
+
+        return this.numbers.map(function (number) {
+            return number * _this3.multiplyBy;
+        });
+    }
+};
+console.log(multiplier.multiply());
---
+++ src/playground/es6-arguments-this.js
@@ -0,0 +1,63 @@
+// arguments objects - no longer bound with arrow functions
+
+const add = function (a, b){ //ES5
+    console.log(arguments); // returns all arguments passed to this function, irregardless to what is listed as an argument. 
+    return a + b;
+};
+console.log(add(9,19));
+
+const addArrow = (a, b) => { // ES6 arrow function
+   //console.log(arguments); Not possible with arrow functions
+    return a + b;
+};
+console.log(addArrow(20,2));
+
+// this keyword - no longer bound
+const user = { 
+    name: 'Sanjeev',
+    cities: ['NYC', 'Queens', 'Miami'],
+    printPlaceLived: function(){ // ES5: since this function is added to the object property, the this is bound to that object
+        console.log(this.name);  
+        console.log(this.cities);
+
+        this.cities.forEach(function (city) { 
+            //console.log(this.name + ' has lived n' + city); // does not work since this anonymous function is not bound to the object
+        });
+    }
+};
+
+console.log(user.printPlaceLived());
+
+const user1 = { 
+    name1: "Sanjeev1",
+    cities1: ['NYC', 'Queens', 'Miami'],
+    printPlaceLived(){ // ES6 syntax for defining a method function
+        this.cities1.forEach((city) => { 
+            console.log(this.name1 + 'has lived in ' + city); // ES6 works since arrow functions inherit the this of the context they are created
+        });
+    }
+};
+
+user1.printPlaceLived();
+
+const user2 = { //using map
+    name2: 'Sanjeev2',
+    cities2: ['NYC', 'Queens', 'Miami'],
+    printPlaceLived(){
+        return this.cities2.map((city)=> this.name2 + ' has lived in ' + city); //map allow you to transform each item in the arry and get a new array back
+    }
+};
+console.log(user2.printPlaceLived());
+
+// CHALLENGE
+const multiplier = {
+    // number - array of numbers
+    numbers: [1,2,3],
+    // multiplyBy - single number 
+    multiplyBy: 2,
+    // Method: Multiply -> returns a array where the numbers have been multiplied
+    multiply(){
+        return this.numbers.map((number) => number * this.multiplyBy); 
+    }
+};
+console.log(multiplier.multiply());
