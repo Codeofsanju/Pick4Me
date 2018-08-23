aceddc6e545987343173011c501a1f9780624f69
Codeofsanju <sanjeev.sharma90@myhunter.cuny.edu>
2018-08-23 11:44:16 -0400

    ES6 Classes II - Subclasses of Classes, Parent method overiding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -50,7 +56,7 @@ var Challenge = function () {
     _createClass(Challenge, [{
         key: 'getDescription',
         value: function getDescription() {
-            return this.name + ' is ' + this.age + ' years old';
+            return this.name + ' is ' + this.age + ' years old.';
         }
     }]);
 
@@ -60,5 +66,54 @@ var Challenge = function () {
 var challengePerson = new Challenge('Challenging Bill', 45);
 var challengePersonNoAge = new Challenge('Challenging Bill2');
 
-console.log(challengePerson.getDescription());
-console.log(challengePersonNoAge.getDescription());
+//console.log(challengePerson.getDescription());
+//console.log(challengePersonNoAge.getDescription());
+
+// Subclass of person class
+
+var Student = function (_Challenge) {
+    _inherits(Student, _Challenge);
+
+    function Student(name, age, major) {
+        _classCallCheck(this, Student);
+
+        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+
+        _this.major = major;
+        return _this;
+    }
+
+    _createClass(Student, [{
+        key: 'getMajor',
+        value: function getMajor() {
+            return this.name + '\'s major is ' + this.major;
+        }
+    }, {
+        key: 'hasMajor',
+        value: function hasMajor() {
+            return !!this.major; // returns false if student has no major
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            // overiding parent class getDescription method 
+            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+            if (this.hasMajor()) {
+                description = description + (' Their major is ' + this.major);
+            }
+            return description;
+        }
+    }]);
+
+    return Student;
+}(Challenge);
+
+var student1 = new Student('Henry Sharma', 25);
+console.log(student1.getDescription());
+console.log(student1.getMajor());
+console.log(student1.hasMajor());
+
+var student2 = new Student('Abinav Sharma', 27, 'Computer Science');
+console.log(student2.getDescription());
+console.log(student2.getMajor());
+console.log(student2.hasMajor());
--- src/playground/es6-classes1.js
+++ src/playground/es6-classes1.js
@@ -27,14 +27,43 @@ class Challenge{
         this.age = age;
     }
     getDescription(){
-        return `${this.name} is ${this.age} years old`;
+        return `${this.name} is ${this.age} years old.`;
     }
     
 }
 const challengePerson = new Challenge('Challenging Bill', 45);
 const challengePersonNoAge = new Challenge('Challenging Bill2');
 
-console.log(challengePerson.getDescription());
-console.log(challengePersonNoAge.getDescription());
+//console.log(challengePerson.getDescription());
+//console.log(challengePersonNoAge.getDescription());
 
+// Subclass of person class
+class Student extends Challenge{
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    getMajor(){
+        return `${this.name}'s major is ${this.major}`;
+    }
+    hasMajor(){
+        return !!this.major; // returns false if student has no major
+    }
+    getDescription(){ // overiding parent class getDescription method 
+        let description = super.getDescription();
+        if(this.hasMajor()){
+            description = description + ` Their major is ${this.major}`;
+        }
+        return description;
+    }
+}
 
+const student1 = new Student('Henry Sharma', 25);
+console.log(student1.getDescription());
+console.log(student1.getMajor());
+console.log(student1.hasMajor());
+
+const student2 = new Student('Abinav Sharma', 27, 'Computer Science');
+console.log(student2.getDescription());
+console.log(student2.getMajor());
+console.log(student2.hasMajor());
