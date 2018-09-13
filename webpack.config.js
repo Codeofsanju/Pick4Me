10c0b05a4f6d6a3f5fe02b3c23711b9f50e6bf9b
Codeofsanju <sanjeev.sharma90@myhunter.cuny.edu>
2018-09-13 10:41:31 -0400

    Setting up babel to work with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/app.js', // for webpack input and output dir
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+
+    module: { // for babel
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
 };
 
